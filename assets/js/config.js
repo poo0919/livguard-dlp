490fd2bb7c603a587f083524e26cceb4d66c2065
erashishji <poonam.k@tagbin.in>
2017-06-28 16:21:10 +0530

    before

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+var rootEndpoint = "http://api.bharatpay.co.in/v1/";
+var apiUrls = {
+	"users": rootEndpoint + "users/",
+	"login": rootEndpoint + "login/",
+	"inventory" : rootEndpoint + "inventory/", 
+}
